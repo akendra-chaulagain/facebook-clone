e3719a1df843aee80036bc26dd4cc51ea2dd9126
Akendra-Chaulagain <yakendrachaulagain31@gmail.com>
2022-05-03 18:20:07 +0545

    loading

---
+++ client/src/components/Loading/Loading copy.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <>
+      <div class="containerLoading">
+        <div class="dash uno"></div>
+        <div class="dash dos"></div>
+        <div class="dash tres"></div>
+        <div class="dash cuatro"></div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
